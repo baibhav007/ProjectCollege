20de95be42ac5edfeda4d11d463d99379084dbe2
baibhav.kumar <baibhavrishu91@gmail.com>
2025-02-02 00:20:58 +0530

    code changes

--- models/hostel.js
+++ models/hostel.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const HostelSchema = new mongoose.Schema({
   name: { type: String, required: true },
   mobile: { type: String, required: true },
-  bedRequired: { type: Number, required: true },
-  visitTime: { type: String, required: true },
-  budget: { type: Number, required: true },
+  bedRequired: { type: Number },
+  visitTime: { type: String },
+  budget: { type: Number },
   recommendedBy: { type: String },
 }, { timestamps: true });
 
-const Hostel = mongoose.models.Hostel || mongoose.model("HostelRequest", HostelSchema);
+const Hostel = mongoose.models.Hostel || mongoose.model("Hostel", HostelSchema);
 
 export default Hostel;
