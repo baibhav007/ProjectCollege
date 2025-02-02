70e8120308b69db7a172db4b8c842ba18ee3c38d
baibhav.kumar <baibhavrishu91@gmail.com>
2025-02-02 19:15:03 +0530

    admin added

--- api/admin.mjs
+++ api/admin.mjs
@@ -1,28 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const AdminJS = require('adminjs');
-const AdminJSExpress = require('@adminjs/express');
-const AdminJSMongoose = require('@adminjs/mongoose');
+import express from 'express';
+import mongoose from 'mongoose';
+import AdminJS from 'adminjs';
+import AdminJSExpress from '@adminjs/express';
+import * as AdminJSMongoose from '@adminjs/mongoose';
 
-// Import your models
-const Student = require('./models/student');
-const Hostel = require('./models/hostel');
+// Import models
+import Student from '../models/student.js';
+import Hostel from '../models/hostel.js';
+
 
 // Connect AdminJS with Mongoose
 AdminJS.registerAdapter(AdminJSMongoose);
 
-// Initialize Express app
 const app = express();
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://baibhavrishu97:esvugto1QitxBn5w@cluster0.2u7yh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => {
-  console.log('MongoDB connected');
-}).catch(err => {
-  console.error('MongoDB connection error:', err);
-});
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Configure AdminJS
 const adminJs = new AdminJS({
@@ -46,7 +43,6 @@ const adminRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
 
 app.use(adminJs.options.rootPath, adminRouter);
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Admin panel is running at http://localhost:${PORT}/admin`);
